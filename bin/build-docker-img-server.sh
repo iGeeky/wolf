62966f8b828b1f6d3ff535634a3ebf1aa35123d1
iGeeky <igeeky.io@gmail.com>
2020-02-29 13:40:39 +0800

    Change scripts

--- bin/build-docker-img-server.sh
+++ bin/build-docker-img-server.sh
@@ -6,7 +6,14 @@ fi
 
 TAG="$1"
 echo "BUILD TAG: $TAG"
-cd ./console && npm install && npm run build:prod && cd ../
-
+echo "BUILD CONSOLE..."
+cd ./console && cnpm install && cnpm run build:prod
+if [ "$?" != "0" ]; then
+  echo "build console failed!"
+  exit 2
+fi
+cd ../
+echo "BUILD SERVER..."
 docker build -t igeeky/wolf-server:$TAG -f ./server/Dockerfile ./server
 docker build -t igeeky/wolf-server:latest -f ./server/Dockerfile ./server
+echo "BUILD SUCCESS."
