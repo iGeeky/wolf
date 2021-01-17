b272c0fea97ead18bcb1c2251c438fb8ec3988c0
iGeeky <igeeky.io@gmail.com>
2021-01-17 19:13:06 +0800

    Add scripts

---
+++ bin/build-docker-img-server-use-cnpm.sh
@@ -0,0 +1,19 @@
+if [ "$#" == "0" ]; then
+  echo "Usage: sh $0 TAG"
+  echo "\tTAG: 1.2.3"
+  exit 1
+fi
+
+TAG="$1"
+echo "BUILD TAG: $TAG"
+echo "BUILD CONSOLE..."
+cd ./console && cnpm install && cnpm run build:prod
+if [ "$?" != "0" ]; then
+  echo "build console failed!"
+  exit 2
+fi
+cd ../
+echo "BUILD SERVER..."
+docker build -t igeeky/wolf-server:$TAG -f ./server/Dockerfile ./server
+docker build -t igeeky/wolf-server:latest -f ./server/Dockerfile ./server
+echo "BUILD SUCCESS."
